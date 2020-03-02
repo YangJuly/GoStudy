52a4a2e02f9cff29e72de69ff463bb5f6901954c
July Yang <756564820@qq.com>
2020-03-02 11:36:02 +0800

    add redis query based on go-redis

--- GoHttpServerOrmDemo/go.mod
+++ GoHttpServerOrmDemo/go.mod
@@ -3,6 +3,7 @@ module GoStudy/GoHttpServerOrmDemo
 go 1.13
 
 require (
+	github.com/go-redis/redis v6.15.7+incompatible
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/go-xorm/xorm v0.7.9
 	github.com/kr/pretty v0.2.0 // indirect
--- GoHttpServerOrmDemo/main/main.go
+++ GoHttpServerOrmDemo/main/main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"GoStudy/GoHttpServerOrmDemo/httpserver"
-	_ "GoStudy/GoHttpServerOrmDemo/models"
+	"github.com/go-redis/redis"
+
+	//"GoStudy/GoHttpServerOrmDemo/httpserver"
+	//_ "GoStudy/GoHttpServerOrmDemo/models"
 	"fmt"
-	_ "github.com/go-sql-driver/mysql"
 	"net/http"
+
+	//_ "github.com/go-sql-driver/mysql"
+	//"net/http"
 )
 
 func main(){
 	fmt.Println("Start")
 
+	//query redis
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	val, err := client.Get("name").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("name", val)
+
 	//httpServer
 	http.HandleFunc("/", httpserver.HandleIndex)
-	err := http.ListenAndServe(":8000", nil)
+	err = http.ListenAndServe(":8000", nil)
 	if err != nil{
 		fmt.Println(err)
 	}
